aa3ad78f4cd8c0ed871a5ab059576cb38919c84b
Ramiro <rhuayller@agetic.gob.bo>
2017-08-17 19:24:54 -0400

    ok

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app">
+    <app-sidenav></app-sidenav>
+    <app-navbar></app-navbar>
     <router-view></router-view>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
   import AppNavbar from '@/common/layout/AppNavbar';
-  import AppSidenav from '@/common/layout/AppSidenav';
+  import AppSidenav from '@/common/layout/AppSidebar';
   import AppFooter from '@/common/layout/AppFooter';
   import { mapState } from 'vuex'
 export default {
